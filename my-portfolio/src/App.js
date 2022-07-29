c052c69846404a747529df0ba824bfadef4c356d
britbolt <blattisaw@gmail.com>
2022-07-29 00:36:59 -0400

    initiate react app

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import 
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+mongo "mongodb+srv://clusterbolt.ingk23h.mongodb.net/budget_tracker" --apiVersion 1 --username britbolt
+G5qqy%21ep698c32k
